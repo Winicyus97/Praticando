a82d4a9e661cb5532d32045bade1a9eb1e0d58ef
DevAlexFR <alexsandro77flores@gmail.com>
2025-03-13 18:35:33 -0300

    TEST

--- Curso_em_video/EX_017.py
+++ Curso_em_video/EX_017.py
@@ -1,12 +1,27 @@
 import math
 
-def calcular_hipotenusa(cateto_oposto, cateto_adjacente):
+def calcular_hipotenusa(cateto_oposto:float, cateto_adjacente:float) -> float:
+    """ Função para retornar a hipotenusa
+
+    Parameters
+    ----------
+    cateto_oposto : float
+        Cateto oposto em formato float
+    cateto_adjacente : float
+        Cateto adjacente em formato float
+
+    Returns
+    -------
+    float
+        Hipotenusa dos catetos de entrada em formato float
+
+    Exemplo:
+    -------
+        .. code-block:: python
+            print(calcular_hipotenusa(2, 2))
+    """
+
     hipotenusa = math.sqrt(cateto_oposto**2 + cateto_adjacente**2)
     return hipotenusa
 
-cateto_oposto=float(input("qual o cateto oposto ?"))
-cateto_adjacente=float(input("qual o cateto adjacente ?"))
-
-hipotenusa = calcular_hipotenusa(cateto_oposto, cateto_adjacente)
-print(f"A hipotenusa é : {hipotenusa}")
-
+print(f"A hipotenusa é : {calcular_hipotenusa(float(input("qual o cateto oposto ?")), float(input("qual o cateto adjacente ?")))}")
